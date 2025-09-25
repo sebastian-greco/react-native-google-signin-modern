6d51e0e6e5cc37c2e639aed584ee9662e0a50078
Sebastian Greco <greco.sebastian@gmail.com>
2025-09-25 02:42:11 +0200

    feat(ios): enhance Google Sign-In module with error handling and configuration validation

--- ios/GoogleSigninModern.mm
+++ ios/GoogleSigninModern.mm
@@ -1,18 +1,284 @@
 #import "GoogleSigninModern.h"
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+#import <React/RCTBridgeModule.h>
+#import <React/RCTLog.h>
+#import <React/RCTUtils.h>
+#import <React/RCTBridge.h>
+
+#ifdef RCT_NEW_ARCH_ENABLED
+#import <React/RCTSurfacePresenter.h>
+#import <React/RCTSurfacePresenterBridgeAdapter.h>
+#import <ReactCommon/RCTTurboModule.h>
+#import <react/renderer/graphics/Float.h>
+#include <memory>
+#endif
+
+#if __has_include(<GoogleSignIn/GoogleSignIn.h>)
+#import <GoogleSignIn/GoogleSignIn.h>
+#define HAS_GOOGLE_SIGNIN 1
+#else
+#define HAS_GOOGLE_SIGNIN 0
+#endif
+
+// Define logging macros if React Native logging isn't available
+#ifndef RCTLogInfo
+#define RCTLogInfo(...) NSLog(__VA_ARGS__)
+#endif
+#ifndef RCTLogWarn
+#define RCTLogWarn(...) NSLog(__VA_ARGS__)
+#endif
+#ifndef RCTLogError
+#define RCTLogError(...) NSLog(__VA_ARGS__)
+#endif
+
+@interface GoogleSigninModern ()
+@property (nonatomic, copy) NSString *webClientId;
+@property (nonatomic, copy) void (^pendingResolve)(id result);
+@property (nonatomic, copy) void (^pendingReject)(NSString *code, NSString *message, NSError *error);
+@property (nonatomic, assign) BOOL signInInProgress;
+@end
 
 @implementation GoogleSigninModern
+
 RCT_EXPORT_MODULE()
 
-- (NSNumber *)multiply:(double)a b:(double)b {
-    NSNumber *result = @(a * b);
+// Error codes (matching Android implementation)
+static NSString * const ERROR_NOT_CONFIGURED = @"NOT_CONFIGURED";
+static NSString * const ERROR_SIGN_IN_IN_PROGRESS = @"SIGN_IN_IN_PROGRESS";
+static NSString * const ERROR_NO_GOOGLE_ACCOUNTS = @"NO_GOOGLE_ACCOUNTS";
+static NSString * const ERROR_SIGN_IN_ERROR = @"SIGN_IN_ERROR";
+static NSString * const ERROR_CONFIGURE_ERROR = @"CONFIGURE_ERROR";
+static NSString * const ERROR_SIGN_OUT_ERROR = @"SIGN_OUT_ERROR";
+static NSString * const ERROR_SIGN_OUT_REQUESTED = @"SIGN_OUT_REQUESTED";
+static NSString * const ERROR_MODULE_DESTROYED = @"MODULE_DESTROYED";
+static NSString * const ERROR_USER_CANCELLED = @"USER_CANCELLED";
 
-    return result;
+- (instancetype)init {
+    if (self = [super init]) {
+        _signInInProgress = NO;
+    }
+    return self;
 }
 
+RCT_EXPORT_METHOD(configure:(NSString *)webClientId
+                  resolve:(RCTPromiseResolveBlock)resolve
+                  reject:(RCTPromiseRejectBlock)reject) {
+    @try {
+        // Validate webClientId format
+        if (!webClientId || webClientId.length == 0) {
+            reject(ERROR_CONFIGURE_ERROR, @"webClientId cannot be blank", nil);
+            return;
+        }
+        
+        if (![webClientId hasSuffix:@".apps.googleusercontent.com"]) {
+            RCTLogWarn(@"webClientId doesn't match expected format: *.apps.googleusercontent.com");
+        }
+        
+#if HAS_GOOGLE_SIGNIN
+        // Configure Google Sign-In
+        GIDConfiguration *config = [[GIDConfiguration alloc] initWithClientID:webClientId];
+        [GIDSignIn sharedInstance].configuration = config;
+        
+        self.webClientId = webClientId;
+        
+        RCTLogInfo(@"Google Sign-In configured successfully");
+        resolve(nil);
+#else
+        reject(ERROR_CONFIGURE_ERROR, @"Google Sign-In SDK not found. Please install GoogleSignIn pod.", nil);
+#endif
+    }
+    @catch (NSException *exception) {
+        RCTLogError(@"Failed to configure Google Sign-In: %@", exception.reason);
+        reject(ERROR_CONFIGURE_ERROR, [NSString stringWithFormat:@"Failed to configure Google Sign-In: %@", exception.reason], nil);
+    }
+}
+
+RCT_EXPORT_METHOD(isPlayServicesAvailable:(RCTPromiseResolveBlock)resolve
+                  reject:(RCTPromiseRejectBlock)reject) {
+    // On iOS, Google Sign-In doesn't require Play Services
+    // Always return true as the GoogleSignIn SDK handles availability
+    resolve(@YES);
+}
+
+RCT_EXPORT_METHOD(signIn:(RCTPromiseResolveBlock)resolve
+                  reject:(RCTPromiseRejectBlock)reject) {
+    @try {
+        RCTLogInfo(@"Sign-in request initiated");
+        
+        if (!self.webClientId) {
+            reject(ERROR_NOT_CONFIGURED, @"Google Sign-In not configured. Call configure() first.", nil);
+            return;
+        }
+        
+        if (self.signInInProgress) {
+            RCTLogWarn(@"Sign-in already in progress, rejecting new request");
+            reject(ERROR_SIGN_IN_IN_PROGRESS, @"Sign-in already in progress", nil);
+            return;
+        }
+        
+        // Store promise callbacks
+        self.pendingResolve = resolve;
+        self.pendingReject = reject;
+        self.signInInProgress = YES;
+        
+#if HAS_GOOGLE_SIGNIN
+        // Get the root view controller
+        UIViewController *rootViewController = RCTKeyWindow().rootViewController;
+        while (rootViewController.presentedViewController) {
+            rootViewController = rootViewController.presentedViewController;
+        }
+        
+        RCTLogInfo(@"Starting Google Sign-In flow");
+        
+        // Start sign-in flow
+        [[GIDSignIn sharedInstance] signInWithPresentingViewController:rootViewController
+                                                            completion:^(GIDSignInResult *result, NSError *error) {
+            dispatch_async(dispatch_get_main_queue(), ^{
+                [self handleSignInResult:result error:error];
+            });
+        }];
+#else
+        [self clearPendingPromiseWithError:ERROR_SIGN_IN_ERROR message:@"Google Sign-In SDK not available"];
+#endif
+        
+    }
+    @catch (NSException *exception) {
+        RCTLogError(@"Exception in signIn: %@", exception.reason);
+        [self clearPendingPromiseWithError:ERROR_SIGN_IN_ERROR 
+                                   message:[NSString stringWithFormat:@"Sign-in failed: %@", exception.reason]];
+    }
+}
+
+#if HAS_GOOGLE_SIGNIN
+- (void)handleSignInResult:(GIDSignInResult *)result error:(NSError *)error {
+    self.signInInProgress = NO;
+    
+    if (error) {
+        RCTLogError(@"Google Sign-In error: %@", error.localizedDescription);
+        
+        // Handle specific error cases
+        if (error.code == kGIDSignInErrorCodeCanceled) {
+            [self clearPendingPromiseWithError:ERROR_USER_CANCELLED message:@"User cancelled the sign-in"];
+        } else if (error.code == kGIDSignInErrorCodeHasNoAuthInKeychain) {
+            [self handleNoAccountsError];
+        } else {
+            NSString *errorMessage = [NSString stringWithFormat:@"Sign-in failed: %@", error.localizedDescription];
+            [self clearPendingPromiseWithError:ERROR_SIGN_IN_ERROR message:errorMessage];
+        }
+        return;
+    }
+    
+    if (!result || !result.user) {
+        [self clearPendingPromiseWithError:ERROR_SIGN_IN_ERROR message:@"No user data received"];
+        return;
+    }
+    
+    // Create response matching Android format
+    GIDGoogleUser *user = result.user;
+    GIDProfileData *profile = user.profile;
+    
+    NSDictionary *userDict = @{
+        @"id": profile.email ?: @"",
+        @"name": profile.name ?: @"",
+        @"email": profile.email ?: @"",
+        @"photo": profile.hasImage ? [profile imageURLWithDimension:120].absoluteString : [NSNull null]
+    };
+    
+    NSDictionary *response = @{
+        @"idToken": user.idToken.tokenString ?: @"",
+        @"user": userDict
+    };
+    
+    RCTLogInfo(@"Google Sign-In successful");
+    if (self.pendingResolve) {
+        self.pendingResolve(response);
+    }
+    [self clearPendingPromise];
+}
+#endif
+
+- (void)handleNoAccountsError {
+    RCTLogInfo(@"No Google accounts available - opening account settings");
+    
+    // On iOS, we can't directly add Google accounts like Android
+    // Simply inform the user to add Google account manually
+    [self clearPendingPromiseWithError:ERROR_NO_GOOGLE_ACCOUNTS 
+                               message:@"Please add a Google account in Settings > Mail > Accounts and try again."];
+}
+
+- (void)signOut:(void (^)(id result))resolve
+       rejecter:(void (^)(NSString *code, NSString *message, NSError *error))reject {
+    @try {
+        RCTLogInfo(@"Sign-out requested");
+        
+        // If there's a pending sign-in, reject it first
+        if (self.pendingReject) {
+            self.pendingReject(ERROR_SIGN_OUT_REQUESTED, @"Sign-out was requested", nil);
+        }
+        
+#if HAS_GOOGLE_SIGNIN
+        // Sign out from Google
+        [[GIDSignIn sharedInstance] signOut];
+#endif
+        
+        // Clear stored state
+        [self clearAllState];
+        
+        RCTLogInfo(@"Sign-out completed successfully");
+        resolve(nil);
+    }
+    @catch (NSException *exception) {
+        RCTLogError(@"Error during sign-out: %@", exception.reason);
+        reject(ERROR_SIGN_OUT_ERROR, [NSString stringWithFormat:@"Sign-out failed: %@", exception.reason], nil);
+    }
+}
+
+- (void)isSignedIn:(void (^)(id result))resolve
+          rejecter:(void (^)(NSString *code, NSString *message, NSError *error))reject {
+    // With modern Google Sign-In, apps should manage their own auth state
+    // Return false to encourage proper state management
+    RCTLogInfo(@"isSignedIn called - returning false (app should manage auth state)");
+    resolve(@NO);
+}
+
+#pragma mark - Helper Methods
+
+- (void)clearPendingPromiseWithError:(NSString *)errorCode message:(NSString *)message {
+    if (self.pendingReject) {
+        self.pendingReject(errorCode, message, nil);
+    }
+    [self clearPendingPromise];
+}
+
+- (void)clearPendingPromise {
+    self.pendingResolve = nil;
+    self.pendingReject = nil;
+    self.signInInProgress = NO;
+}
+
+- (void)clearAllState {
+    [self clearPendingPromise];
+    self.webClientId = nil;
+}
+
+#pragma mark - Module Lifecycle
+
+- (void)invalidate {
+    RCTLogInfo(@"Module invalidated - cleaning up resources");
+    if (self.pendingReject) {
+        self.pendingReject(ERROR_MODULE_DESTROYED, @"Module was destroyed", nil);
+    }
+    [self clearAllState];
+}
+
+#pragma mark - TurboModule
+
+#ifdef RCT_NEW_ARCH_ENABLED
 - (std::shared_ptr<facebook::react::TurboModule>)getTurboModule:
-    (const facebook::react::ObjCTurboModule::InitParams &)params
-{
+    (const facebook::react::ObjCTurboModule::InitParams &)params {
     return std::make_shared<facebook::react::NativeGoogleSigninModernSpecJSI>(params);
 }
+#endif
 
 @end
